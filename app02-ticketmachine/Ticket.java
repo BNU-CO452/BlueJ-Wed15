f24b3e24ae11a8b28a37aeccab1b169d743b7b56
Derek Peacock <derek.peacock@yahoo.com>
2020-10-14 16:11:11 +0100

    Create Ticket with fields

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Ticket here.
+ *
+ * @author Derek Peacock
+ * @version (a version number or a date)
+ */
+public class Ticket
+{
+    private String destination;
+    
+    // this is in pence
+    private int price;
+    
+    /**
+     * Constructor for objects of class Ticket
+     */
+    public Ticket()
+    {
+        price = 220;
+        destination = "Aylesbury";
+    }
+    
+    public Ticket(String destination, int price)
+    {
+        this.price = price;
+        this.destination = destination;
+    }
+    
+    public void setPrice(int price)
+    {
+        this.price = price;
+    }
+
+}
