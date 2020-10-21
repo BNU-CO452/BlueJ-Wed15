f19e3feaf6e323119c50be665c1f8df7fa4ea388
Derek Peacock <derek.peacock@yahoo.com>
2020-10-21 16:34:45 +0100

    started app03

---
+++ app03-studentGrades/Module.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+    private String title;
+    
+    private String codeNo;
+    
+    private int mark;
+    
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module(String title, String codeNo)
+    {
+        mark = 0;
+        this.title = title;
+        this.codeNo = codeNo;
+    }
+
+}
--- app03-studentGrades/Student.java
+++ app03-studentGrades/Student.java
@@ -16,6 +16,8 @@ public class Student
     // the amount of credits for study taken so far
     private int credits;
     
+    private Course course;
+    
     /**
      * Create a new student with a given name and ID number.
      */
@@ -26,6 +28,11 @@ public class Student
         credits = 0;
     }
 
+    public void setCourse(Course course)
+    {
+        this.course = course;
+    }
+    
     /**
      * Return the full name of this student.
      */
@@ -83,5 +90,6 @@ public class Student
     public void print()
     {
         System.out.println(name + ", student ID: " + id + ", credits: " + credits);
+        course.print();
     }
 }
